c6f217f34ebec190a2950ab6fb48c6cbb10348cf
codeis8520 <codeis8520@gmail.com>
2026-04-01 20:29:14 +0900

    fix: 회원가입 페이지 분리

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 import { login, signup } from './actions';
+import Link from 'next/link';
 
 export default function LoginPage() {
   return (
@@ -39,12 +40,12 @@ export default function LoginPage() {
             >
               로그인
             </button>
-            <button
-              formAction={signup}
-              className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            <Link
+              href="/login/signup"
+              className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 text-center"
             >
               회원가입
-            </button>
+            </Link>
           </div>
         </form>
       </div>
